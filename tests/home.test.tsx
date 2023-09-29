f05a3dd19fe5b2dbffdedeba9df38de0c3724ac8
Dmytro <dmytro.osiychuk@gmail.com>
2023-09-29 13:12:18 +0200

    Add vitest tests

---
+++ tests/home.test.tsx
@@ -0,0 +1,38 @@
+import { render, screen } from '@testing-library/react'
+import { vi } from 'vitest'
+import Page from '../app/page'
+
+vi.mock('@clerk/nextjs', () => {
+  // Create an mockedFunctions object to match the functions we are importing from the @nextjs/clerk package in the ClerkComponent component.
+  const mockedFunctions = {
+    auth: () =>
+      new Promise((resolve) =>
+        resolve({ userId: 'user_2NNEqL2nfg4ndJqAHwEfxC' })
+      ),
+    ClerkProvider: ({ children }) => <div>{children}</div>,
+    useUser: () => ({
+      isSignedIn: true,
+      user: {
+        id: 'user_2NNEqL2nrIRdJ194ndJqAHwEfxC',
+        fullName: 'Charles Harris',
+      },
+    }),
+  }
+
+  return mockedFunctions
+})
+
+vi.mock('next/font/google', () => {
+  return {
+    Inter: () => ({ className: 'inter' }),
+  }
+})
+
+test(`Home`, async () => {
+  render(await Page())
+  expect(
+    screen.getByText(
+      'This is the best app for tracking the mood of journal entries.'
+    )
+  ).toBeTruthy()
+})
